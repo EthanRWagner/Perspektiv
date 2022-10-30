ac725daf4b93681455245ae6d4d5a65cbd631e4f
OzzY <ewagne02@calpoly.edu>
2022-10-30 16:45:42 -0700

    feed outline, dropdown menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RegisterPage from "./RegisterPage";
 import MyApp from "./MyApp";
+import Feed from "./Feed";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import "./App.css";
 //<button onClick={navigateToRegister}>Register</button>
@@ -14,14 +15,22 @@ function App() {
 			<div>
 				<BrowserRouter basename="/">
 				<nav>
-					<ul>
-						<li>
+					<div className="dropdown">
 							<Link to="/">PERSPEKTIV</Link>
-						</li>
-						<li>
-							<Link to="/register">Register Here</Link>
-						</li>
-					</ul>
+						<div className="dropdown-content">
+							<ul className="no-bullets">
+								<li>
+									<Link to="/register">Register</Link>
+								</li>
+								<li>
+									<Link to="/login">Login</Link>
+								</li>
+								<li>
+									<Link to="/<user>feed">Feed</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</nav>
 					<Routes>
 						<Route
@@ -34,6 +43,18 @@ function App() {
 							path="/register"
 							element={
 								<RegisterPage />
+							}
+						/>
+						<Route
+							path="/login"
+							element={
+								<RegisterPage />
+							}
+						/>
+						<Route
+							path="/<user>feed"
+							element={
+								<Feed />
 							}
 						/>
 					</Routes>
