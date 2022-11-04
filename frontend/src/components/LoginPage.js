1f38c5a39311b6c1718c9cd41360dfb473320c4c
liam <liammshaw@gmail.com>
2022-11-04 12:38:44 -0700

    fixed login -> backend connection

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,33 +1,21 @@
 import axios from 'axios'; 
 import Login from "./Login"
 import "../css/App.css";
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 const port = 8675;
 
 function LoginPage (){
-    const [characters, setCharacters] = useState([]);
 
     function updateList(person) { 
-        makePostCall(person).then( result => {
-        if (result && result.status === 201)
-        setCharacters([...characters, result.data] );
+        makeGetCall(person).then( result => {
+            console.log(result.status);
+        if (result && result.status === 404)
+        console.log("Login Failed");
         });
     }
 
-    async function fetchAll(){
-        try {
-        const response = await axios.get(`htp://localhost:${port}/users`);
-        return response.data.users_list;
-        }
-        catch (error){
-        //We're not handling errors. Just logging into the console.
-        console.log(error); 
-        return false;         
-        }
-    }
-
-    async function makePostCall(person){
+    async function makeGetCall(person){
         try {
         const response = await axios.get(`http://localhost:${port}/users`, person);
         console.log(person)
@@ -38,13 +26,6 @@ function LoginPage (){
         return false;
         }
     }
-
-    useEffect(() => {
-        fetchAll().then( result => {
-        if (result)
-            setCharacters(result);
-        });
-    }, [] );
         
     return (
         <div className="container">
