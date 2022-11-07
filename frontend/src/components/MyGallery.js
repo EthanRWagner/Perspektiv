f17ad804c013ba36da204ab11986d565ca3c00ef
OzzY <ewagne02@calpoly.edu>
2022-11-06 16:41:57 -0800

    search page done

---
+++ frontend/src/components/MyGallery.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import "../css/SearchBar.css";
+
+// const images = [
+//     {
+//       original: '../img/search-icon.png',
+//       thumbnail: '../img/search-icon.png',
+//     },
+//     {
+//         original: '../img/search-icon.png',
+//         thumbnail: '../img/search-icon.png',
+//     },
+//     {
+//         original: '../img/search-icon.png',
+//         thumbnail: '../img/search-icon.png',
+//     },
+//   ];
+
+class MyGallery extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            images: [
+            {
+                original: 'https://picsum.photos/id/1018/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1018/250/150/',
+            },
+            {
+                original: 'https://picsum.photos/id/1015/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1015/250/150/',
+            },
+            {
+                original: 'https://picsum.photos/id/1019/1000/600/',
+                thumbnail: 'https://picsum.photos/id/1019/250/150/',
+            },
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <ImageGallery 
+                    items={this.state.images}
+                    autoPlay="true"
+                    showPlayButton="false"
+                    showBullets="true"
+                    showFullscreenButton="false"
+                    />
+            </div>
+            
+        );
+    }
+}
+
+export default MyGallery;
--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import "../css/SearchPage.css";
+import MyGallery from './MyGallery';
 
 function SearchPage (){
+
     return (
-        <t>Search Page</t>
+        <div className='search-content'>
+            <div className='featured-frame'>
+                <div  className='featured-box'>
+                    <t className='box-headings'>Featured HodgePodges</t>
+                    <MyGallery />
+                </div>
+
+                <div className='featured-box'>
+                    <t className='box-headings'>Featured People</t>
+                    <MyGallery />
+                </div>
+
+                <div className='featured-box'>
+                    <t className='box-headings'>Featured Posts</t>
+                    <MyGallery />
+                </div>
+            </div>
+
+            <div className='line-break'></div>
+
+            <div className='results-frame'>
+                <div  className='results-box'>
+                    <t className='box-headings'>Results In HodgePodges</t>
+                    <div className='sample-result'></div>
+                </div>
+
+                <div className='results-box'>
+                    <t className='box-headings'>Results In People</t>
+                    <div className='sample-result'></div>
+                </div>
+
+                <div className='results-box'>
+                    <t className='box-headings'>Results In Tags</t>
+                    <div className='sample-result'></div>
+                </div>
+            </div>
+        </div>
     );
 
 }
