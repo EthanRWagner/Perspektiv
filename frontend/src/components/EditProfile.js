1bf5720c17973b636329439a368c0aa65bd95d38
liam <liammshaw@gmail.com>
2022-11-14 10:47:05 -0800

    working on front end functionality of the edit profile page

--- frontend/src/components/EditProfile.js
+++ frontend/src/components/EditProfile.js
@@ -7,6 +7,66 @@ import Input from "react-validation/build/input";
 // import PropTypes from "prop-types";
 
 function EditProfile(){
+    const [user, setUser] = useState({
+        username: "", email: "", password: "", confirmPassword: ""
+    });
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+        if (name === "username") {
+            setUser({
+                username: value, 
+                email: user['email'], 
+                password: user['password'], 
+                confirmPassword: user['confirmPassword']
+            }); 
+        }
+        else if(name === "email"){
+            setUser({
+                username: user['username'], 
+                email: value, 
+                password: user['password'], 
+                confirmPassword: user['confirmPassword']
+            }); 
+        }
+        else if(name === "password"){
+            setUser({
+                username: user['username'], 
+                email: user['email'], 
+                password: value, 
+                confirmPassword: user['confirmPassword']}); 
+        }
+        else {
+            setUser({
+                username: user['username'], 
+                email: user['email'], 
+                password: user['password'], 
+                confirmPassword: value}); 
+        }
+    }
+
+    function updateUser(user){
+        attemptFind(user).then(result => {
+            if (result && result.status != 201) {
+                console.log("USER NOT FOUND");
+            }
+        })
+    }
+
+    async function attemptFind(person){
+        try {
+            const response = await axios.post(`http://localhost:${port}/signin`, person);
+            return response;
+        }
+        catch (error) {
+            return error;
+        }
+    }
+
+    function EditFrom(){
+        updateUser(user)
+    }
+
     return (
         <div className='col-md-12'>
             {/* <h2 className='logo-center'>User&apos;s Profile</h2>
@@ -24,17 +84,15 @@ function EditProfile(){
                             type="text"
                             name="username"
                             id="username"
-                            />
-                            {/*onChange={handleChange}/>*/}
+                            onChange={handleChange}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="username">New Email</label>
                         <Input
                             type="text"
-                            name="username"
-                            id="username"
-                            />
-                            {/*onChange={handleChange}/>*/}
+                            name="email"
+                            id="email"
+                            onChange={handleChange}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">New Password</label>
@@ -42,17 +100,15 @@ function EditProfile(){
                             type="text"
                             name="password"
                             id="password"
-                            />
-                            {/*onChange={handleChange}/>*/}
+                            onChange={handleChange}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Confirm New Password</label>
                         <Input
                             type="text"
-                            name="password"
-                            id="password"
-                            />
-                            {/*onChange={handleChange}/>*/}
+                            name="confirmPassword"
+                            id="confirmPassword"
+                            onChange={handleChange}/>
                     </div>
                     <div className="form-group">
                         <input type="button" value="Update Information" />
